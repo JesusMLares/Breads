4692fba6dff66b91b571d57b3026b0e3bca010f1
JesusMLares <twinlares2@hotmail.com>
2024-05-20 19:24:32 -0700

    Used .populate

--- views/edit.jsx
+++ views/edit.jsx
@@ -1,7 +1,8 @@
 const React = require("react");
 const Default = require("./layouts/default");
+const { options } = require("../controllers/breads_controller");
 
-function Edit({ bread }) {
+function Edit({ bread, bakers }) {
   return (
     <Default>
       <h2>Edit a bread</h2>
@@ -18,12 +19,11 @@ function Edit({ bread }) {
         <input type="text" name="image" id="image" defaultValue={bread.image} />
         <label htmlFor="baker">Baker</label>
         <select name="baker" id="baker" defaultValue={bread.baker}>
-          <option value="Rachel">Rachel</option>
-          <option value="Monica">Monica</option>
-          <option value="Joey">Joey</option>
-          <option value="Chandler">Chandler</option>
-          <option value="Ross">Ross</option>
-          <option value="Phoebe">Phoebe</option>
+          {bakers.map((baker)=> {
+            return(
+              <option value={baker.id} key={baker.id}>{baker.name}</option>
+            )
+          })}
         </select>
         <label htmlFor="hasGluten">Has Gluten?</label>
         <input
